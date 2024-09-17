5788bd3782dcf3cd9267a22af0df8fdd6f31a9f0
SV <SV@PM19-PC19>
2024-09-17 15:08:24 +0700

    Add project files.

---
+++ 23DH111636_LETRANDANGKHOA/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _23DH111636_LETRANDANGKHOA
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
